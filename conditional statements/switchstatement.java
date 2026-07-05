41514d14bb7b2321c715ffe808dbcd36be2a4b23
shubham vishwakarma <shubham10137@gmail.com>
2026-07-05 20:31:14 +0530

    Add files via upload

---
+++ conditional statements/switchstatement.java
@@ -0,0 +1,21 @@
+public class switchstatement {
+    public static void main(String[] args) {
+        int n = 4;
+        switch (n) {
+            case 1:
+                System.out.println("samosa");
+                break;
+            case 2:
+                System.out.println("momos");
+                break;
+
+            case 3:
+                System.out.println("burger");
+                break;
+
+            default:
+                System.out.println("it's your nightmare");
+
+        }
+    }
+}
