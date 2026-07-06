b3c40f0fbf9a151930c6947213cfc874ac733668
shubham vishwakarma <shubham10137@gmail.com>
2026-07-06 22:09:50 +0530

    Add files via upload

---
+++ loops/demo.java
@@ -0,0 +1,9 @@
+public class demo {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println(100 + "shubham is best codder in world");
+            counter++;
+        }
+    }
+}
