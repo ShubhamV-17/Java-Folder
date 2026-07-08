b1897c92f1f6d0397f3ab740013b76506fd3b887
shubham vishwakarma <shubham10137@gmail.com>
2026-07-08 22:36:35 +0530

    Add files via upload

---
+++ loops/no1to10print.java
@@ -0,0 +1,9 @@
+public class no1to10print {
+    public static void main(String[] args) {
+        int counter = 1;
+        while (counter <= 10) {
+            System.out.print(counter + " ");
+            counter++;
+        }
+    }
+}
